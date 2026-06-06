b1624f119d62cba79acbe25f89a7c1ff10d05a14
Fanglai <laura@FLMacBook-Air.local>
2026-06-06 14:44:30 +0200

    changed url

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,7 +1,7 @@
 const newQuoteButton = document.getElementById("newQuote");
 
 newQuoteButton.addEventListener("click", () => {
-    fetch("http://localhost:3000/")
+    fetch("http://pqd08avykibslel1wvl8txuz.178.105.39.91.sslip.io")
         .then(response => response.text())
         .then(newQuote => {
             const quote = document.getElementById("quote");
